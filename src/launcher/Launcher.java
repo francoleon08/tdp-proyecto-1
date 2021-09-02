d61cc3f3674bfb11beaaf65ba0e1ef8a9cb56767
francoleon08 <francoleon284@gmail.com>
2021-09-02 11:12:41 -0300

    Primer commit proyecto terminado

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,16 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	SimplePresentationScreen a = new SimplePresentationScreen(new Student(12345,"UNS","Juan Carlos","juan.carlos.uns@cs.uns.edu.ar","https://github.com/juancarlosuns","/images/User.png"));
+            	a.setVisible(true);
             }
         });
     }
